61b03645b2bfb3c28514ad3cb2cfcdf0bb94f4aa
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-10-30 04:38:05 +0300

    database copy

--- app/src/main/java/com/asimodabas/movie_database/MainActivity.kt
+++ app/src/main/java/com/asimodabas/movie_database/MainActivity.kt
@@ -2,10 +2,33 @@ package com.asimodabas.movie_database
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.info.sqlitekullanimihazirveritabani.DatabaseCopyHelper
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        veritabaniKopyala()
+    }
+
+    fun veritabaniKopyala() {
+
+        val db = DatabaseCopyHelper(this)
+
+        try {
+            db.createDataBase()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        try {
+            db.openDataBase()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
