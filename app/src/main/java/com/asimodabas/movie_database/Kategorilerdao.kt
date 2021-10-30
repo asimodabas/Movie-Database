ffe249da52c4c4b094db83eaaa386f57f63ce46a
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-10-30 04:57:24 +0300

    database +

--- app/src/main/java/com/asimodabas/movie_database/Kategorilerdao.kt
+++ app/src/main/java/com/asimodabas/movie_database/Kategorilerdao.kt
@@ -1,4 +1,24 @@
 package com.asimodabas.movie_database
 
 class Kategorilerdao {
+
+    fun tumKategoriler(vt: DatabaseHelper): ArrayList<Kategoriler> {
+
+        val kategoriListe = ArrayList<Kategoriler>()
+        val db=vt.writableDatabase
+        val cursor = db.rawQuery("SELECT * FROM kategoriler ", null)
+
+        while (cursor.moveToNext()) {
+            val kategori = Kategoriler(
+                cursor.getInt(cursor.getColumnIndex("kategori_id")),
+                cursor.getString(cursor.getColumnIndex("kategori_ad")))
+
+            kategoriListe.add(kategori)
+
+        }
+
+        return kategoriListe
+
+    }
+
 }
--- app/src/main/java/com/asimodabas/movie_database/MainActivity.kt
+++ app/src/main/java/com/asimodabas/movie_database/MainActivity.kt
@@ -2,6 +2,7 @@ package com.asimodabas.movie_database
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.info.sqlitekullanimihazirveritabani.DatabaseCopyHelper
 import java.lang.Exception
 
@@ -11,6 +12,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         veritabaniKopyala()
+
+        val vt=DatabaseHelper(this)
+        val liste=Kategorilerdao().tumKategoriler(vt)
+
+        for (k in liste){
+            Log.e("Kategori Adı:",k.kategori_ad)
+            Log.e("Kategori Id:",k.kategori_id.toString())
+        }
     }
 
     fun veritabaniKopyala() {
